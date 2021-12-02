68b5e942d4a7662c78a36f196abc81c582f12b9f
Oier Lauzirika Zarrabeitia <oierlauzi@bizkaia.eu>
2021-12-02 11:05:10 +0100

    Added parameter validation to the form

--- cistem/protocols/protocol_3d_classification.py
+++ cistem/protocols/protocol_3d_classification.py
@@ -23,7 +23,8 @@
 # **************************************************************************
 
 from pyworkflow.protocol.constants import LEVEL_ADVANCED
-from pyworkflow.protocol.params import EnumParam, MultiPointerParam, PointerParam, FloatParam, IntParam, BooleanParam, StringParam
+from pyworkflow.protocol.params import EnumParam, MultiPointerParam, PointerParam, FloatParam, IntParam, BooleanParam
+from pyworkflow.protocol.params import LT, LE, GE, GT, Range
 from pwem.protocols import ProtClassify3D
 from pyworkflow.constants import BETA
 
@@ -56,7 +57,7 @@ class CistemProt3DClassification(ProtClassify3D):
         form.addSection(label='Refinement')
         form.addParam('cycleCount', IntParam, label='Cycle Count',
                         help='Number of refinement cycles to be executed',
-                        default=1)
+                        default=1, validators=[GE(1)])
         form.addParam('refinement_type', EnumParam, choices=['Local', 'Global'], label='Refinement type',
                         default=0)
         group = form.addLine('Refinement parameters', help='Parameters to be refined',
@@ -72,34 +73,34 @@ class CistemProt3DClassification(ProtClassify3D):
         group.addParam('refine_yShift', BooleanParam, label='Y shift',
                         default=True, expertLevel=LEVEL_ADVANCED)
         form.addParam('refine_lowResLimit', FloatParam, label='Low resolution limit (Å)',
-                        default=225.0, expertLevel=LEVEL_ADVANCED)
+                        default=225.0, validators=[GT(0)], expertLevel=LEVEL_ADVANCED)
         form.addParam('refine_outerMaskRadius', FloatParam, label='Outer mask radius (Å)',
-                        default=97.5, expertLevel=LEVEL_ADVANCED)
+                        default=97.5, validators=[GE(0)], expertLevel=LEVEL_ADVANCED)
         form.addParam('refine_innerMaskRadius', FloatParam, label='Inner mask radius (Å)',
-                        default=0.0, expertLevel=LEVEL_ADVANCED)
+                        default=0.0, validators=[GE(0)], expertLevel=LEVEL_ADVANCED)
         form.addParam('refine_signedCcResLimit', FloatParam, label='Signed CC resolution limit (Å)',
                         default=0.0, expertLevel=LEVEL_ADVANCED)
         form.addParam('refine_usedPercentage', FloatParam, label='Percentage used (%)',
-                        default=100.0, expertLevel=LEVEL_ADVANCED)
+                        default=100.0, validators=[Range(0, 100)], expertLevel=LEVEL_ADVANCED)
         group = form.addGroup('Global search', expertLevel=LEVEL_ADVANCED,
                                 condition='refinement_type == 1')
         group.addParam('refine_globalMaskRadius', FloatParam, label='Global mask radius (Å)',
-                        default=120.0, expertLevel=LEVEL_ADVANCED)
+                        default=120.0, validators=[GT(0)], expertLevel=LEVEL_ADVANCED)
         group.addParam('refine_numResults', IntParam, label='Number of results to refine',
-                        default=20, expertLevel=LEVEL_ADVANCED)
+                        default=20, validators=[GT(0)], expertLevel=LEVEL_ADVANCED)
         group.addParam('refine_refineInputParameters', BooleanParam, label='Refine input parameters',
                         default=True, expertLevel=LEVEL_ADVANCED)
         group.addParam('refine_angularStep', FloatParam, label='Angular search step (º)',
-                        default=35.26, expertLevel=LEVEL_ADVANCED)
+                        default=35.26, validators=[GT(0)], expertLevel=LEVEL_ADVANCED)
         group.addParam('refine_xRange', FloatParam, label='Search range in X (Å)',
-                        default=22.5, expertLevel=LEVEL_ADVANCED)
+                        default=22.5, validators=[GE(0)], expertLevel=LEVEL_ADVANCED)
         group.addParam('refine_yRange', FloatParam, label='Search range in Y (Å)',
-                        default=22.5, expertLevel=LEVEL_ADVANCED)
+                        default=22.5, validators=[GE(0)], expertLevel=LEVEL_ADVANCED)
 
 
         form.addSection(label='Classification')
         form.addParam('classification_hiresLimit', FloatParam, label='High Resolution Limit (Å)',
-                        default=30.0)
+                        default=30.0, validators=[GT(0)])
         form.addParam('classification_enableFocus', BooleanParam, label='Enable focused classification',
                         default=False, expertLevel=LEVEL_ADVANCED)
         group = form.addLine('Focus sphere', 
@@ -113,7 +114,7 @@ class CistemProt3DClassification(ProtClassify3D):
         group.addParam('classification_sphereZ', FloatParam, label='Center Z (Å)',
                         default=0, expertLevel=LEVEL_ADVANCED)
         group.addParam('classification_radius', FloatParam, label='Radius (Å)',
-                        default=10.0, expertLevel=LEVEL_ADVANCED)
+                        default=10.0, validators=[GT(0)], expertLevel=LEVEL_ADVANCED)
 
 
         form.addSection(label='CTF refinement')
@@ -121,28 +122,28 @@ class CistemProt3DClassification(ProtClassify3D):
                         default=False, expertLevel=LEVEL_ADVANCED)
         form.addParam('ctf_range', FloatParam, label='Defocus search range (Å)',
                         condition='ctf_enable is True',
-                        default=500.0, expertLevel=LEVEL_ADVANCED)
+                        default=500.0, validators=[GE(0)], expertLevel=LEVEL_ADVANCED)
         form.addParam('ctf_step', FloatParam, label='Defocus search step (Å)',
                         condition='ctf_enable is True',
-                        default=50.0, expertLevel=LEVEL_ADVANCED)
+                        default=50.0, validators=[GT(0)], expertLevel=LEVEL_ADVANCED)
 
 
         form.addSection(label='Reconstruction')
         form.addParam('reconstruction_score2weight', FloatParam, label='Score to weight constant (Å²)',
-                        default=2.0, expertLevel=LEVEL_ADVANCED)
+                        default=2.0, validators=[GT(0)], expertLevel=LEVEL_ADVANCED)
         form.addParam('reconstruction_adjustScore4Defocus', BooleanParam, label='Adjust score for defocus',
                         default=True, expertLevel=LEVEL_ADVANCED)
         form.addParam('reconstruction_scoreThreshold', FloatParam, label='Score threshold',
-                        default=0.0, expertLevel=LEVEL_ADVANCED)
+                        default=0.0, validators=[GE(0)], expertLevel=LEVEL_ADVANCED)
         form.addParam('reconstruction_resLimit', FloatParam, label='Resolution limit (Å²)',
-                        default=0.0, expertLevel=LEVEL_ADVANCED)
+                        default=0.0, validators=[GE(0)], expertLevel=LEVEL_ADVANCED)
         form.addParam('reconstruction_enableAutoCrop', BooleanParam, label='Enable auto cropping images',
                         default=False, expertLevel=LEVEL_ADVANCED)
         form.addParam('reconstruction_enableLikelihoodBlurring', BooleanParam, label='Enable likelihood blurring',
                         default=False, expertLevel=LEVEL_ADVANCED)
         form.addParam('reconstruction_smoothingFactor', FloatParam, label='Smoothing factor',
                         condition='reconstruction_enableLikelihoodBlurring is True',
-                        default=10.0, expertLevel=LEVEL_ADVANCED)
+                        default=10.0, validators=[GT(0)], expertLevel=LEVEL_ADVANCED)
 
 
         form.addSection(label='Masking')
@@ -152,22 +153,32 @@ class CistemProt3DClassification(ProtClassify3D):
                         help='3D mask used to crop the output volume',
                         condition='masking_enableAuto is False', default=None)
         form.addParam('masking_edgeWidth', FloatParam, label='Edge width (Å)',
-                        condition='masking_volume is not None', default=10.0,
-                        expertLevel=LEVEL_ADVANCED)
+                        condition='masking_volume is not None', 
+                        default=10.0, validators=[GE(0)], expertLevel=LEVEL_ADVANCED)
         form.addParam('masking_outsideWeight', FloatParam, label='Outside weight',
-                        condition='masking_volume is not None', default=0.0,
-                        expertLevel=LEVEL_ADVANCED)
+                        condition='masking_volume is not None', 
+                        default=0.0, validators=[GE(0)], expertLevel=LEVEL_ADVANCED)
         form.addParam('masking_enableLpfOutsideMask', BooleanParam, label='Use a LPF outside the mask',
-                        condition='masking_volume is not None', default=False,
-                        expertLevel=LEVEL_ADVANCED)
+                        condition='masking_volume is not None', 
+                        default=False, expertLevel=LEVEL_ADVANCED)
         form.addParam('masking_lpfResolution', FloatParam, label='LPF resolution (Å)',
-                        condition='masking_enableLpfOutsideMask is True', default=20.0,
-                        expertLevel=LEVEL_ADVANCED)
+                        condition='masking_enableLpfOutsideMask is True', 
+                        default=20.0, validators=[GT(0)], expertLevel=LEVEL_ADVANCED)
 
 
     # --------------------------- INSERT steps functions ----------------------
     def _insertAllSteps(self):
-        self._insertFunctionStep("createOutputStep")
+        # Shorthands for variables
+        nCycles = self.cycleCount.get()
+        
+        # Perform refine, reconstruct and merge steps repeatedly
+        for i in range(nCycles):
+            self._insertFunctionStep('refineStep', i)
+            self._insertFunctionStep('reconstructStep', i)
+            self._insertFunctionStep('mergeStep', i)
+
+        # Generate the output
+        self._insertFunctionStep('createOutputStep')
 
     # --------------------------- STEPS functions -----------------------------
     def refineStep(self, i):
